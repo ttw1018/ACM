4baca2239fb34f11d6f77fc6d031e339570b91f8
TianwenTang <627747903@qq.com>
2020-12-02 20:32:55 +0800

    change the file struct

---
+++ cf/contest/1457/e/e.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <map>
+#include <cstring>
+#include <vector>
+#include <algorithm>
+#include <queue>
+#include <set>
+
+using namespace std;
+typedef long long ll;
+
+// const int maxn = 2e5 + 5;
+// const int mod = 1e9 + 7;
+
+
+int main () {
+    
+
+
+
+    return 0;
+}
+
