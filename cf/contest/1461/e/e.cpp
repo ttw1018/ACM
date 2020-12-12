9b4f261ee5026fc053631cc50b5ea515b25fce61
TianwenTang <627747903@qq.com>
2020-12-12 02:09:07 +0000

    update

--- cf/contest/1461/e/e.cpp
+++ cf/contest/1461/e/e.cpp
@@ -20,20 +20,28 @@ const int maxn = 1e5 + 5;
 int main() {
   ll k, l, r, t, x, y;
   cin >> k >> l >> r >> t >> x >> y;
-  if (x > y) {
-    ll cnt;
-    if (k + y <= r) {
-      cnt = (k - l) / (x - y);
-    } else {
-      cnt = (k - l) / x + (k - (k - l) / x * x + (k - l) / x * y) / x;
-    }
-    if (cnt >= t) {
-      cout << "Yes" << endl;
-    } else {
-      cout << "No" << endl;
-    }
-    return 0;
-  }
+
+	if (y < x) {
+		if (k + y <= r) k += r;
+
+		ll cnt = (k - l) / x;
+
+		k -= (k - l) / x * x;
+
+		k += y * cnt;
+
+		cnt += (k - l) / x;
+
+		if (cnt >= t) {
+			cout << "Yes" << endl;
+		}
+		else {
+			cout << "No" << endl;
+		}
+
+
+		return 0;
+	}
 
   ll now = k;
   ll ans = 0;
@@ -42,10 +50,9 @@ int main() {
   if (now + y <= r)
     now += y;
 
+
   for (ll i = 0; i < x;) {
     ll add = (now - l) / x;
-    if (y - add <= ans)
-      break;
     ans += add;
     now -= add * x;
     ll cc = min(add, (r - now) / y);
@@ -56,6 +63,7 @@ int main() {
     now += cc * y;
     i += cc;
   }
+
 
   if (flag || ans >= t) {
     cout << "Yes" << endl;
